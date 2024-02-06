be00a184248c490b69fabca03b96468ba65914a3
Abhishek Suman <abhisheksuman413@gmail.com>
2024-02-06 23:14:00 +0530

    Muiltiple Recursion Call

--- 01_Learn the basics/Lec_01_05_ Learn Basic Recursion/sample.cpp
+++ 01_Learn the basics/Lec_01_05_ Learn Basic Recursion/sample.cpp
@@ -2,76 +2,31 @@
 using namespace std;
 
 
-// void fun(int cnt, int size, string &str3)
-// {
-//     if (cnt >= size / 2)
-//     {
-//         return;
-//     }
-//     else
-//     {
-//         swap(str3[cnt], str3[size - cnt - 1]);
-//         fun(cnt + 1, size, str3);
-//     }
-// }
-
-// bool cheack(string str3, string str2)
-// {
-//     if (str3 == str2)
-//     {
-//         return 1;
-//     }
-//     else
-//     {
-//         return 0;
-//     }
-// }
-//     bool isPalindrome(string str) {
-
-//     int cnt = 0;
-//     int size = str.size();
-
-//     string str3;
-//      for(int i =0; i<str.length();i++){
-//         if(isalnum(str[i])){
-//             str3.push_back(tolower(str[i]));
-//         }
-//     }
-//     string str2 = str3;
-//     fun(cnt, size, str3);
-//     return cheack(str3, str2);
-        
-//     }
-
-bool cheack(int cnt , int size , string &str){
-    if(cnt>=size/2) return true;
-    if(str[cnt]!= str[size - cnt - 1]) return false;
-    return cheack( cnt+1 , size , str);
+bool fun2(int n){
+    return true;
 }
 
-bool isPalindrome(string str){
+void fun(int abhi[]){
 
-    int cnt = 0;
-    int size = str.size();
-    string new_str;
-
-
-    for(int i =0; i<str.length();i++){
-        if(isalnum(str[i])){
-            new_str.push_back(tolower(str[i]));
-        }
-    }
-    cout<<new_str;
-
-    return cheack(cnt,size,new_str);
+    abhi[0]=0;
+    abhi[1]=0;
+    abhi[2]=0;
+    
 }
+
+
+
 
 int main()
 {
-    string str = "A man, a plan, a canal: Panama";
-    // char str2 = '65';
-    string str3;
-    cout<<isPalindrome(str);
+    int n;
+    int abhi[3];
+    abhi[0]=1;
+    abhi[1]=2;
+    abhi[2]=3;
+    fun(abhi);
+    cout<<fun2(n);
+
 
     return 0;
 }
